3b622eaccd60fe7aa37f0562f9b2d1e35faa682e
iMac <imac@imacs-imac.local>
2016-12-08 18:23:18 +0300

    Initial Commit

---
+++ tuedu/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  tuedu
+//
+//  Created by iMac on 11/22/1437 AH.
+//  Copyright © 1437 lhd. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
